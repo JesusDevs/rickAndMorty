26dbe7b6e67f358e1f707b980c79c48e6b4ae719
Leonardo Jesus Rodriguez Orellana <ljrodrio@MacBook-Pro-de-Soporte-IT.local>
2022-09-02 13:45:49 -0400

    se agrega metodo para captar dats clave valor

--- app/src/main/java/com/rickandmorty/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/rickandmorty/MyFirebaseMessagingService.kt
@@ -2,20 +2,32 @@ package com.rickandmorty
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.rickandmorty.ui.searchCharacter.SearchCharacterFragment
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class MyFirebaseMessagingService: FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        Log.d("TAG", "From: " + remoteMessage.from)
-        Log.d("TAG", "Notification Message Body: " + remoteMessage.notification?.body)
+        remoteMessage.data.isNotEmpty().let {
+            remoteMessage.data.values.forEach {clave->
+                Log.d("TAG", "Message data payload: $it")
+                if (clave == "url") {
+                    startActivity(Intent(this, SearchCharacterFragment::class.java))
+                } else if (clave == "Morty") {
+                    Handler(Looper.getMainLooper()).post {
+                        Toast.makeText(this, "Morty", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
+            }
+        }
         Handler(Looper.getMainLooper()).post {
             Toast.makeText(this, remoteMessage.notification?.body, Toast.LENGTH_LONG).show()
         }
